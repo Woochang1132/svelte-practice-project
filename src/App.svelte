649d9add4606b4e48373662c0eed3877d20cc34f
woochang <show4031132@gmail.com>
2025-03-28 14:03:29 +0900

    [svelte] > 이벤트 기능으로 좋아요 버튼 구성하기 [1/2] 완료료

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
   import logo1 from './assets/svelte.svg'
-  let title = '영화제목2'
-  let year = 2024
-  let category = '액션'
-  let textRed = "color: red;"
-
   const datas = [
+    {
+      title: '고질라 X 콩: 뉴 엠파이어',
+      year: 2024,
+      category: '어드벤처, 액션, SF'
+    },
     {
       title: '고질라 X 콩: 뉴 엠파이어',
       year: 2024,
@@ -16,23 +16,24 @@
       year: 2024,
       category: '액션'
     },
-  ]
+  ];
+  let likeCount = 0;
+  const handleLike = () => {
+    likeCount += 1;
+  }
 
-  const foods = ['pizza', 'burger', 'sushi', 'pasta'];
 </script>
 
 <main>
-  {#each foods as food, i}
-    <p>{i}: {food}</p>
-  {/each}
   <h1>영화정보</h1>
   <img src={logo1} alt="">
   <img src="/vite.svg" alt="">
   {#each datas as data}
   <div>
-    <h3 class="bg-yellow" style={textRed}>{data.title}</h3>
+    <h3 class="bg-yellow">{data.title}</h3>
     <p>개봉: {data.year}</p>
     <p>장르: {data.category}</p>
+    <button on:click={handleLike}>좋아요 {likeCount}</button>
   </div>
   {/each}
 
@@ -41,5 +42,7 @@
 <style>
 .bg-yellow{
   background: gold;
+  padding: 10px;
+  color: #333;
 }
 </style>
