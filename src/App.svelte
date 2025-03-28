749b080f54681cca5942e7a5c1d91d69a4f9cba0
woochang <show4031132@gmail.com>
2025-03-28 14:44:40 +0900

    [svelte] > movieInfo 페이지 css 추가완료료

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,25 @@
   import logo1 from './assets/svelte.svg'
   const datas = [
     {
-      title: '고질라 X 콩: 뉴 엠파이어',
+      title: '파묘',
       year: 2024,
       category: '어드벤처, 액션, SF',
-      likeCount: 0
+      likeCount: 0,
+      imgUrl : '/images/파묘.jpg'
     },
     {
       title: '고질라 X 콩: 뉴 엠파이어',
       year: 2024,
       category: '어드벤처, 액션, SF',
-      likeCount: 0
+      likeCount: 0,
+      imgUrl : '/images/고질라X콩.jpg'
     },
     {
       title: '듄 파트2',
       year: 2024,
       category: '액션',
-      likeCount: 0
+      likeCount: 0,
+      imgUrl : '/images/듄-파트2.jpg'
     },
   ];
   const handleLike = (i) => {
@@ -26,16 +29,21 @@
 
 </script>
 
-<main>
+<main class="container">
   <h1>영화정보</h1>
   <img src={logo1} alt="">
   <img src="/vite.svg" alt="">
   {#each datas as data, i}
-  <div>
-    <h3 class="bg-yellow">{data.title}</h3>
-    <p>개봉: {data.year}</p>
-    <p>장르: {data.category}</p>
-    <button on:click={() => {handleLike(i)}}>좋아요 {data.likeCount}</button>
+  <div class="item">
+    <figure>
+      <img src={data.imgUrl} alt={data.title}>
+    </figure>
+    <div class="info">
+      <h3 class="bg-yellow">{data.title}</h3>
+      <p>개봉: {data.year}</p>
+      <p>장르: {data.category}</p>
+      <button on:click={() => {handleLike(i)}}>좋아요 {data.likeCount}</button>
+    </div>
   </div>
   {/each}
 
@@ -46,5 +54,25 @@
   background: gold;
   padding: 10px;
   color: #333;
+}
+.item {
+  width: 100%;
+  border: 1px solid #ccc;
+  display: flex;
+  margin-bottom: 20px;
+  padding: 1rem;
+}
+
+.item figure{
+  width: 30%;
+  margin-right: 1rem;
+}
+
+.item img{
+  width: 100%;
+}
+
+.item .info{
+  width: 100%;
 }
 </style>
