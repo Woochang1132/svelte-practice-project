85e1de577361b621fc77d16fec222a5d3f1e6e80
woochang <show4031132@gmail.com>
2025-03-28 14:17:36 +0900

    [svelte] > 이벤트 기능, click 활용하기

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,24 @@
     {
       title: '고질라 X 콩: 뉴 엠파이어',
       year: 2024,
-      category: '어드벤처, 액션, SF'
+      category: '어드벤처, 액션, SF',
+      likeCount: 0
     },
     {
       title: '고질라 X 콩: 뉴 엠파이어',
       year: 2024,
-      category: '어드벤처, 액션, SF'
+      category: '어드벤처, 액션, SF',
+      likeCount: 0
     },
     {
       title: '듄 파트2',
       year: 2024,
-      category: '액션'
+      category: '액션',
+      likeCount: 0
     },
   ];
-  let likeCount = 0;
-  const handleLike = () => {
-    likeCount += 1;
+  const handleLike = (i) => {
+    datas[i].likeCount += 1;
   }
 
 </script>
@@ -28,12 +30,12 @@
   <h1>영화정보</h1>
   <img src={logo1} alt="">
   <img src="/vite.svg" alt="">
-  {#each datas as data}
+  {#each datas as data, i}
   <div>
     <h3 class="bg-yellow">{data.title}</h3>
     <p>개봉: {data.year}</p>
     <p>장르: {data.category}</p>
-    <button on:click={handleLike}>좋아요 {likeCount}</button>
+    <button on:click={() => {handleLike(i)}}>좋아요 {data.likeCount}</button>
   </div>
   {/each}
 
