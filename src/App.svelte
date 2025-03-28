cae582b03c7d7714314d5cefa02bfc5a1acc211d
woochang <show4031132@gmail.com>
2025-03-28 13:36:04 +0900

    [svelte] > svelte의 반복문 => each를 활용하기기

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,45 @@
 <script>
+  import logo1 from './assets/svelte.svg'
+  let title = '영화제목2'
+  let year = 2024
+  let category = '액션'
+  let textRed = "color: red;"
 
+  const datas = [
+    {
+      title: '고질라 X 콩: 뉴 엠파이어',
+      year: 2024,
+      category: '어드벤처, 액션, SF'
+    },
+    {
+      title: '듄 파트2',
+      year: 2024,
+      category: '액션'
+    },
+  ]
+
+  const foods = ['pizza', 'burger', 'sushi', 'pasta'];
 </script>
 
 <main>
-  <h1>Hello Svelte App</h1>
+  {#each foods as food, i}
+    <p>{i}: {food}</p>
+  {/each}
+  <h1>영화정보</h1>
+  <img src={logo1} alt="">
+  <img src="/vite.svg" alt="">
+  {#each datas as data}
+  <div>
+    <h3 class="bg-yellow" style={textRed}>{data.title}</h3>
+    <p>개봉: {data.year}</p>
+    <p>장르: {data.category}</p>
+  </div>
+  {/each}
+
 </main>
 
 <style>
-
+.bg-yellow{
+  background: gold;
+}
 </style>
