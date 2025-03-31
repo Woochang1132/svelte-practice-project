585541f406ba9ea32acf082dd57f73de691e990b
woochang <show4031132@gmail.com>
2025-03-31 10:30:03 +0900

    [svelte] > transition 효과 적용, 컴포넌트 추가가

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
   import Navbar from './lib/components/Navbar.svelte';
   import Modal from './lib/components/Modal.svelte';
   import Movies from './lib/components/Movies.svelte';
+  import AddTansition from './lib/components/AddTansition.svelte';
+
 
 
   let movieData = $state(datas);
@@ -22,46 +24,18 @@
 
   // 이벤트창 표시 여부
   let isEvent = $state(true);
+  const AddTansitionFunc = () => {
+    isEvent = false
+  }
+
 </script>
 
 <Navbar />
-<div class={isEvent ? 'event show' : 'event'}>
-  <p>NETPLIX 강렬한 운명의 드라마, 경기크리처</p>
-  <button>X</button>
-</div>
-
+<!-- <div class={isEvent ? 'event show' : 'event'}> -->
+<AddTansition isEvent={isEvent} AddTansitionFunc={AddTansitionFunc}/>
 <Movies movieData={movieData} handleLike={handleLike} openModal={openModal}/>
 
 {#if isModal}
   <Modal movieData={movieData} {selectedMovie} {closeModal}/>
 {/if}
-<style>
-.event{
-  width: 100%;
-  background: #666;
-  padding: 5px 1rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: #fff;
-  text-align: center;
-  margin-bottom: 1em;
-
-  /* 창이 보이지 않게 */
-  max-height: 0px;
-  opacity: 0;
-  overflow: hidden;
-  transition: all 0.4s;
-}
-
-  /* 기본 창이 보이게 */
-  .show{
-    opacity: 1;
-    max-height: 100px;
-  }
-
-  .event button{
-    padding: 2px;
-  }
-
-</style>
+<style></style>
---
+++ src/lib/components/AddTansition.svelte
@@ -0,0 +1,44 @@
+<script>
+      import { fade, fly } from 'svelte/transition';
+      export let isEvent; //전달받은 transition 이벤트 여부
+      export let AddTansitionFunc;
+</script>
+
+{#if isEvent}
+  <div in:fly={{y:-400, duration:1000}} out:fade class='event show'>
+    <p>NETPLIX 강렬한 운명의 드라마, 경기크리처</p>
+    <!-- svelte-ignore event_directive_deprecated -->
+    <button on:click={() => {AddTansitionFunc()}}>X</button>
+  </div>
+{/if}
+
+
+<style>
+ .event{
+  width: 100%;
+  background: #666;
+  padding: 5px 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 1em;
+
+  /* 창이 보이지 않게 */
+  max-height: 0px;
+  opacity: 0;
+  overflow: hidden;
+  transition: all 0.4s;
+}
+
+  /* 기본 창이 보이게 */
+  .show{
+    opacity: 1;
+    max-height: 100px;
+  }
+
+  .event button{
+    padding: 2px;
+  }
+</style>
