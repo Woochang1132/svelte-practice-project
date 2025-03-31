bdb8a8ceca580bc098882b9d3d74d2bd0fa58297
woochang <show4031132@gmail.com>
2025-03-31 09:20:20 +0900

    [svelte] > 동적 class 다루기 [1/2]

--- src/App.svelte
+++ src/App.svelte
@@ -19,13 +19,49 @@
     isModal = true;
     selectedMovie = i;
   }
+
+  // 이벤트창 표시 여부
+  let isEvent = $state(true);
 </script>
 
 <Navbar />
+<div class={isEvent ? 'event show' : 'event'}>
+  <p>NETPLIX 강렬한 운명의 드라마, 경기크리처</p>
+  <button>X</button>
+</div>
+
 <Movies movieData={movieData} handleLike={handleLike} openModal={openModal}/>
 
 {#if isModal}
   <Modal movieData={movieData} {selectedMovie} {closeModal}/>
 {/if}
 <style>
+.event{
+  width: 100%;
+  background: #666;
+  padding: 5px 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 1em;
+
+  /* 창이 보이지 않게 */
+  max-height: 0px;
+  opacity: 0;
+  overflow: hidden;
+  transition: all 0.4s;
+}
+
+  /* 기본 창이 보이게 */
+  .show{
+    opacity: 1;
+    max-height: 100px;
+  }
+
+  .event button{
+    padding: 2px;
+  }
+
 </style>
